56f00483bbf9ebad5e7cc21c7b15fc1466b994ee
Yuri Gonçalves <111998370+Yurig19@users.noreply.github.com>
2023-07-09 17:59:51 -0300

    criando e estilizando card

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import './Collaborator.css'
+
+const Collaborator = () => {
+    return (
+        <div className='collaborator'>
+            <div className='header'>
+                <img src='https://github.com/Yurig19.png' alt='Imagem do colaborador' />
+            </div>
+            <div className='footer'>
+                <h4>Yuri Gonçlaves</h4>
+                <h5>Auxiliar Administrativo</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Collaborator
--- src/components/Department/index.js
+++ src/components/Department/index.js
@@ -1,9 +1,11 @@
+import Collaborator from '../Collaborator'
 import './Department.css'
 
 const Department = (props) => {
     return (
         <section className='department' style={{ backgroundColor: props.backgroundColor }}>
             <h3 style={{ borderColor: props.cardColor }}>{props.name}</h3>
+            <Collaborator />
         </section>
     )
 }
